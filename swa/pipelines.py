a1c191e182d01c431f65db8cf024284e63f4b7a9
ChrisLauinger <chrislauinger@gmail.com>
2016-01-16 19:01:31 -0800

    added MongoDB Pipeline

--- swa/pipelines.py
+++ swa/pipelines.py
@@ -1,40 +1,28 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/topics/item-pipeline.html
+import pymongo
 
-'''
-from items import Fare
-from scrapy import signals
-from scrapy.exceptions import DropItem
-from datetime import datetime as dt
-import sqlite3 
+class MongoPipeline(object):
 
-class CheckDuplicatesPipeline(object):
-	def __init__(self):
-		self.seen = set()
+    def __init__(self, mongo_server, mongo_port, mongo_db):
+        self.mongo_server = mongo_server
+        self.mongo_port = mongo_port
+        self.mongo_db = mongo_db
 
-	def process_item(self, item, spider):
-		if ( isinstance(item, Fare) and item['flight'] in self.seen):
-			raise DropItem("Duplicate caught.")	
-		else:
-			self.seen.add(item['flight'])
-			return item	
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_server=crawler.settings.get('MONGODB_SERVER'),
+            mongo_port=crawler.settings.get('MONGODB_PORT'),
+            mongo_db=crawler.settings.get('MONGODB_DB'), 
+        )
 
-class InsertDBPipeline(object):
-	def __init__(self):
-		self.conn = sqlite3.connect('swa.db')
-		self.c = self.conn.cursor()
-		
-	def process_item(self, item, spider):
-		query = "INSERT INTO flights VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-		data = (dt.now(), item["date"], item["origin"], item["destination"], 
-				str(item["flight"]), item["arrive"], item["depart"], 
-				int(item["price"]), int(item["stops"]),
-				str(item["connectingArpts"]) )
-		if ( isinstance(item, fare) ):
-			self.c.execute(query, data)
-			self.conn.commit()
-		self.conn.close()	
-			
-'''
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_server, self.mongo_port)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+    	collection = spider.origin + "_" + spider.destination
+        self.db[collection].insert(dict(item))
+        return item
