d9b562829886adac537d7718ae99c6a58759edc0
Chris Lauinger <chrislauinger@gmail.com>
2016-02-29 20:40:17 +0000

    added timers

--- pythonServerScraper.py
+++ pythonServerScraper.py
@@ -2,14 +2,20 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import sys, os
 import subprocess
 import urlparse
+import time 
 
 class MyHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
 		length = int(self.headers.getheader('content-length'))
 		field_data = self.rfile.read(length)
 		fields = urlparse.parse_qs(field_data)
+		print(fields)
 		command = 'scrapy crawl southwestFare -a fromCity=%s -a toCity=%s -a days=1 -a startDate="%s"' % (fields['origin'][0], fields['destination'][0], fields['date'][0])
-		subprocess.call(command ,shell = True)
+		print(command)
+		a = time.time()
+		subprocess.call(command ,shell = True) #slow step
+		print("crawl time: " + str(time.time() - a))
+		sys.stdout.flush()
 		self.send_response(200)
 		self.send_header("Content-type", "")
 		self.send_header("Access-Control-Allow-Origin", "*")
